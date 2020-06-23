a724efcb84359bdbd15f955a36e7186cbefcb0cd
sbrenner132 <samuelbrenner@brandeis.edu>
2020-06-22 21:34:05 -0400

    finalized security issues with db password

--- db/db.js
+++ db/db.js
@@ -2,28 +2,26 @@ const mongoose = require('mongoose');
 const fs = require("fs");
 const path = require("path");
 
-let filename = path.join(__dirname, "../DBPassword.txt");
-
-fs.readFile(filename, (err, data) => {
-    if (err) throw err;
-    console.log(data.toString());
-});
-
-const databaseURI = "mongodb+srv://samuelbrenner:Q8aGT682tyjEKb@@cluster0-mesbk.mongodb.net/FamilyServer?retryWrites=true&w=majority"
-
-mongoose.connect(databaseURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
 let db = mongoose.connection;
 
-db.on("error", function (err) {
-    console.log("Mongoose Error: ", err);
-});
+fs.readFile("/mnt/c/Development/Servers/DB_Passwords/FamilyServer.txt", (error, data) => {
+    if (error) throw error;
+    let pass = data.toString();
 
-db.once("open", function () {
-    console.log("Mongoose connection successful.");
+    const databaseURI = "mongodb+srv://samuelbrenner:" + pass + "@cluster0-mesbk.mongodb.net/FamilyServer?retryWrites=true&w=majority"
+
+    mongoose.connect(databaseURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    db.on("error", function (err) {
+        console.log("Mongoose Error: ", err);
+    });
+
+    db.once("open", function () {
+        console.log("Mongoose connection successful.");
+    });
 });
 
 module.exports = db;
