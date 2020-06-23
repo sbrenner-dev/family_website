1c044a4913aeaab9bc51d551d32f89f6df760119
sbrenner132 <samuelbrenner@brandeis.edu>
2020-06-22 20:37:42 -0400

    security changes

--- db/db.js
+++ db/db.js
@@ -1,4 +1,14 @@
 const mongoose = require('mongoose');
+const fs = require("fs");
+const path = require("path");
+
+let filename = path.join(__dirname, "../DBPassword.txt");
+
+fs.readFile(filename, (err, data) => {
+    if (err) throw err;
+    console.log(data.toString());
+});
+
 const databaseURI = "mongodb+srv://samuelbrenner:Q8aGT682tyjEKb@@cluster0-mesbk.mongodb.net/FamilyServer?retryWrites=true&w=majority"
 
 mongoose.connect(databaseURI, {
